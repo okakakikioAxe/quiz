6b63e3cff81736d026c3d192160f0541d2e5e4a1
Lukman M <lukmanmuhamadilham@gmail.com>
2021-05-23 19:28:14 +0700

    tambah rute quiz dan aboutus

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PageController::class, 'index']);
+Route::get('/quiz', [PageController::class, 'quiz']);
+Route::get('/aboutus', [PageController::class, 'aboutus']);
