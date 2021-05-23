90e22c82832d078c2d039f36d7e4fb8100031822
Lukman M <lukmanmuhamadilham@gmail.com>
2021-05-23 19:30:28 +0700

    edit controller dan rename file contact us

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,4 +9,10 @@ class PageController extends Controller
     public function index(){
         return view('home');
     }
+    public function quiz(){
+        return view('quiz');
+    }
+    public function aboutus(){
+        return view('aboutus');
+    }
 }
