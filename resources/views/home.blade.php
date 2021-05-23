392b4902d3c261ea3fa5ef8b4fa5518fd6716e81
Lukman M <lukmanmuhamadilham@gmail.com>
2021-05-23 19:13:28 +0700

    tambah file home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+@extends('template/main')
+
+@section('judul','home')
+
+@section('konten')
+    <div class="container-fluid">
+        <img src="quiz.png" class="mw-100" alt="">
+    </div>
+
+    <br>
+    <br>
+    <div class="container text-center">
+        <figure>
+            <blockquote class="blockquote">
+              <p>“For the things we have to learn before we can do them, we learn by doing them.”</p>
+            </blockquote>
+            <figcaption class="blockquote-footer">
+              <cite title="Source Title">Aristotle</cite>
+            </figcaption>
+        </figure>
+    </div>
+    <br>
+    <br>
+    <div class="container text-center">
+        <p>Wanna try now?</p>
+    </div>
+
+    <div class="container text-center">
+        <button onclick="" type="button" style="width: 300px;height: 80px;font-size: 30px;" class="btn btn-primary">Lets Go</button>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="container text-center">
+        <hr>
+    </div>
+@endsection
