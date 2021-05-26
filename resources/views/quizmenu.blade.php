51c148fa3bd0cc70066f4ff76bd607070325ac03
Lukman M <lukmanmuhamadilham@gmail.com>
2021-05-26 17:13:55 +0700

    tambah file quizmenu

---
+++ resources/views/quizmenu.blade.php
@@ -0,0 +1,57 @@
+@extends('template.main')
+
+@section('judul','quiz menu')
+
+@section('konten')
+    <br>
+    <br>
+    <div class="container-fluid text-center">
+        <div class="row">
+            <div class="col-md p-4">
+                <a href="#">
+                    <img src="{{asset('gambar/bahasa indonesia.png')}}" class="gambar rounded" alt="bahasa indonesia">
+                </a>
+            </div>
+            <div class="col-md p-4">
+                <a href="#">
+                    <img src="{{asset('gambar/bahasa inggris.png')}}" class="gambar rounded border-1" alt="bahasa inggris" >
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg p-4">
+                <a href="https://www.github.com/">
+                    <img src="{{asset('gambar/biologi.png')}}" class="gambar rounded" alt="biologi">
+                </a>
+            </div>
+            <div class="col-lg p-4">
+                <a href="https://www.facebook.com/">
+                    <img src="{{asset('gambar/kimia.png')}}" class="gambar rounded" alt="kimia">
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg p-4">
+                <a href="https://www.google.com/">
+                    <img src="{{asset('gambar/matematika.png')}}" class="gambar rounded" alt="matematika">
+                </a>
+            </div>
+            <div class="col-lg p-4">
+                
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+@endsection
+
+@section('style')
+    .gambar{
+        width: 98%;
+        height: auto;
+        transition: transform 0.5s;
+    }
+    .gambar:hover{
+          transform: scale(1.03);
+    }
+@endsection
