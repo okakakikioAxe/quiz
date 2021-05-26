399d88efc8497f72890ca1f5ecf663ea3092cd72
Lukman M <lukmanmuhamadilham@gmail.com>
2021-05-26 18:19:04 +0700

    edit layout file aboutus

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -5,102 +5,62 @@
 @section('konten')
     <br>
     <br>
-
+    <br>
+    <br>
     <div class="container">
         <div class="row">
-            <div class="col text-center">
+            <div class="col-md text-center">
                 <img src="{{asset('gambar/foto.png')}}" class="rounded-circle mx-auto d-block" alt="..." style="width: 60%;height: auto;">
                 <br>
                 <h4>Lukman Muhamad Ilham</h4>
             </div>
-            <div class="col">
+            <div class="col-md">
+                
+                <div class="row m-4">
+                  <h5>Tentang saya</h5>
+                  <p>Namaku Lukman Muhamad Ilham, lahir di Kediri, 23 Maret 2001. Sekarang ini berstatus mahasiswa Universitas 17 Agustus 1945 Surabaya. Hobiku adalah membuat program dan bermain youtube.</p>
+                </div>
+                
+                <div class="row m-4">
+                  <h5>Hubungi saya : </h5>
+                </div>
+                <div class="row m-4">
+                  <div class="col-2">
+                    <img src="{{asset('gambar/phone.png')}}" class="rounded d-block " alt="..." style="display: block;max-width: 25px;height: auto;">
+                  </div>
+                  <div class="col-8">
+                    <h5 style="float: left;">082245851632</h5>
+                  </div>
+                </div>
 
+                <div class="row m-4">
+                  <div class="col-2">
+                    <img src="{{asset('gambar/email.png')}}" class="rounded d-block " alt="..." style="display: block;max-width: 25px;height: auto;">
+                  </div>
+                  <div class="col-8">
+                    <h5 style="float: left;">lukmanmuhamadilham@gmail.com</h5>
+                  </div>
+                </div>
+
+                <div class="row m-4 text-center">
+                  <h7>Follow me on :</h7>
+                </div>
+
+                <div class="row m-4">
+                  <div class="col">
+                    <a href="https://www.facebook.com/lukman.m.16">
+                      <img src="{{asset('gambar/facebook.png')}}" class="rounded m-2 float-end" alt="..." style="max-width: 35px;height: auto;">
+                    </a>
+                  </div>
+                  <div class="col">
+                    <a href="https://www.instagram.com/lukman_10101/">
+                      <img src="{{asset('gambar/instagram.png')}}" class="rounded m-2 float-start" alt="..." style="max-width: 35px;height: auto;">
+                    </a>
+                  </div>
+                </div>
             </div>
         </div>
     </div>
-    <div class="container text-center">
-        
-        <br>
-        <br>
-        <div class="container">
-            <div class="row">
-              <div class="col">
-                
-              </div>
-              <div class="col-5">
-                <div class="container">
-                  <div class="row">
-                    <div class="col-2">
-                      <img src="{{asset('gambar/phone.png')}}" class="rounded d-block " alt="..." style="display: block;max-width: 25px;height: auto;">
-                    </div>
-                    <div class="col">
-                      <h5 style="float: left;">082245851632</h5>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col">
-                
-              </div>
-            </div>
-        </div>
-        <br>
-        <div class="container">
-            <div class="row">
-              <div class="col">
-                
-              </div>
-              <div class="col-5">
-                <div class="container">
-                  <div class="row">
-                    <div class="col-2">
-                      <img src="{{asset('gambar/email.png')}}" class="rounded d-block " alt="..." style="display: block;max-width: 25px;height: auto;">
-                    </div>
-                    <div class="col">
-                      <h5 style="float: left;">lukmanmuhamadilham@gmail.com</h5>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col">
-                
-              </div>
-            </div>
-        </div>
-        <br>
-        <div class="container text-center">
-          <div class="row">
-            <div class="col">
-              Follow me :
-            </div>
-          </div>
-        </div>
-        <br>
-        <div class="container text-center">
-            <div class="row">
-              <div class="col">
-                
-              </div>
-              <div class="col">
-                <div class="container text-center">
-                  <div class="row">
-                    <div class="col">
-                      <a href="https://www.facebook.com/lukman.m.16">
-                        <img src="{{asset('gambar/facebook.png')}}" class="rounded d-inline-block m-2" alt="..." style="max-width: 35px;height: auto;">
-                      </a>
-                    </div>
-                    <div class="col">
-                      <a href="https://www.instagram.com/lukman_10101/">
-                        <img src="{{asset('gambar/instagram.png')}}" class="rounded d-inline-block m-2" alt="..." style="max-width: 35px;height: auto;">
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col">
-                
-              </div>
-            </div>
-        </div>
-    </div>
+    <br>
+    <br>
 @endsection
