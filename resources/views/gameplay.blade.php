08565748d0b37fc47967deb53d41a685b5eef607
Lukman M <lukmanmuhamadilham@gmail.com>
2021-05-26 20:15:55 +0700

    tambah file gameplay

---
+++ resources/views/gameplay.blade.php
@@ -0,0 +1,66 @@
+@extends('template.main')
+
+@section('judul','gameplay')
+
+@section('konten')
+<br>
+    <br>
+    <br>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-md-8" style="width: 100%;height: auto;">
+               <!--- <img src="kucing.jpg" class="img-fluid"  alt="kucing" ></img> -->
+            </div>
+            <div class="col"></div>
+        </div>
+    </div>
+    <br>
+    <br>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-md-8" style="background-color: rgb(230, 229, 229);">
+                <h4>Question</h4>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia doloremque minus porro tempora repudiandae temporibus sint! Ducimus maiores quis magnam asperiores, ad corporis, assumenda ipsum minus reprehenderit, suscipit qui dolore!</p>
+            </div>
+            <div class="col"></div>
+        </div>
+    </div>
+    <br>
+    <br>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-md-8">
+                <div class="row">
+                    <div class="col m-3">
+                        <button type="button" class="btn btn-info float-start" style="height: 100%;width: 100%;">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil soluta harum hic officiis, vel id accusamus accusantium animi. Porro quo commodi totam assumenda eum qui sequi accusamus delectus, quae accusantium.</button>
+                    </div>
+                    <div class="col m-3">
+                        <button type="button" class="btn btn-info float-end" style="height: 100%;width: 100%;">Menjaganya</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col"></div>
+        </div>
+
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-md-8">
+                <div class="row">
+                    <div class="col m-3">
+                        <button type="button" class="btn btn-info float-start" style="height: 100%;width: 100%;">Menjaganya tetap panas</button>
+                    </div>
+                    <div class="col m-3">
+                        <button type="button" class="btn btn-info float-end" style="height: 100%;width: 100%;">Menjaganya </button>
+                    </div>
+                </div>
+            </div>
+            <div class="col"></div>
+        </div>
+        
+    </div>
+    <br>
+    <br>
+@endsection
